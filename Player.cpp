6474905290f24f697f2a78f9d315fa29a5730738
TuanAnh-P <1tuananhp@gmail.com>
2023-09-30 10:10:08 -0400

    Testing CLion

--- Player.cpp
+++ Player.cpp
@@ -1,55 +1,55 @@
-#include "Player.h"
-
-Player::Player(const std::string& playerName) : playerName(playerName) {}
-
-void Player::addTerritory(Territory* territory) {
-    territories.push_back(territory);
-}
-
-bool Player::removeTerritory(Territory* territory) {
-    for (int i = 0; i < territories.size(); ++i) {
-        if (territories[i] == territory) {
-            territories.erase(territories.begin() + i);
-            return true; // Territory successfully removed
-        }
-    }
-    return false; // Territory not found
-}
-
-const std::vector<Territory*>& Player::getTerritories() const {
-    return territories;
-}
-
-void Player::addCard(Card* card) {
-    cards.push_back(card);
-}
-
-bool Player::removeCard(Card* card) {
-    for (int i = 0; i < cards.size(); ++i) {
-        if (cards[i] == card) {
-            cards.erase(cards.begin() + i);
-            return true; // Territory successfully removed
-        }
-    }
-    return false; // Territory not found
-}
-
-const std::vector<Card*>& Player::getCards() const {
-    return cards;
-}
-
-void Player::issueOrder(Order* order) {
-    orders.push_back(order);
-}
-
-std::vector<Territory*> Player::toDefend() {
-    // Return an arbitrary list of territories
-    std::vector<Territory*> defendTerritories;
-    return defendTerritories;
-}
-
-std::vector<Territory*> Player::toAttack() {
-    // Return an arbitrary list of territories
-    std::vector<Territory*> attackTerritories;
-    return attackTerritories;
-}
+//#include "Player.h"
+//
+//Player::Player(const std::string& playerName) : playerName(playerName) {}
+//
+//void Player::addTerritory(Territory* territory) {
+//    territories.push_back(territory);
+//}
+//
+//bool Player::removeTerritory(Territory* territory) {
+//    for (int i = 0; i < territories.size(); ++i) {
+//        if (territories[i] == territory) {
+//            territories.erase(territories.begin() + i);
+//            return true; // Territory successfully removed
+//        }
+//    }
+//    return false; // Territory not found
+//}
+//
+//const std::vector<Territory*>& Player::getTerritories() const {
+//    return territories;
+//}
+//
+//void Player::addCard(Card* card) {
+//    cards.push_back(card);
+//}
+//
+//bool Player::removeCard(Card* card) {
+//    for (int i = 0; i < cards.size(); ++i) {
+//        if (cards[i] == card) {
+//            cards.erase(cards.begin() + i);
+//            return true; // Territory successfully removed
+//        }
+//    }
+//    return false; // Territory not found
+//}
+//
+//const std::vector<Card*>& Player::getCards() const {
+//    return cards;
+//}
+//
+//void Player::issueOrder(Order* order) {
+//    orders.push_back(order);
+//}
+//
+//std::vector<Territory*> Player::toDefend() {
+//    // Return an arbitrary list of territories
+//    std::vector<Territory*> defendTerritories;
+//    return defendTerritories;
+//}
+//
+//std::vector<Territory*> Player::toAttack() {
+//    // Return an arbitrary list of territories
+//    std::vector<Territory*> attackTerritories;
+//    return attackTerritories;
+//}
--- Player.h
+++ Player.h
@@ -1,38 +1,38 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include <vector>
-#include <string>
-#include "Territory.h"
-#include "Order.h"
-#include "Card.h"
-
-class Player {
-public:
-    Player(const std::string& playerName);
-
-    // Methods to manage territories
-    void addTerritory(Territory* territory);
-    bool removeTerritory(Territory* territory);
-    const std::vector<Territory*>& getTerritories() const;
-
-    // Methods to manage cards
-    void addCard(Card* card);
-    bool removeCard(Card* card);
-    const std::vector<Card*>& getCards() const;
-
-    // Methods for issuing orders
-    void issueOrder(Order* order);
-
-    // Methods to decide which territories to defend and attack
-    std::vector<Territory*> toDefend();
-    std::vector<Territory*> toAttack();
-
-private:
-    std::string playerName;
-    std::vector<Territory*> territories;
-    std::vector<Card*> cards;
-    std::vector<Order*> orders;
-};
-
-#endif
+//#ifndef PLAYER_H
+//#define PLAYER_H
+//
+//#include <vector>
+//#include <string>
+//#include "Territory.h"
+//#include "Order.h"
+//#include "Card.h"
+//
+//class Player {
+//public:
+//    Player(const std::string& playerName);
+//
+//    // Methods to manage territories
+//    void addTerritory(Territory* territory);
+//    bool removeTerritory(Territory* territory);
+//    const std::vector<Territory*>& getTerritories() const;
+//
+//    // Methods to manage cards
+//    void addCard(Card* card);
+//    bool removeCard(Card* card);
+//    const std::vector<Card*>& getCards() const;
+//
+//    // Methods for issuing orders
+//    void issueOrder(Order* order);
+//
+//    // Methods to decide which territories to defend and attack
+//    std::vector<Territory*> toDefend();
+//    std::vector<Territory*> toAttack();
+//
+//private:
+//    std::string playerName;
+//    std::vector<Territory*> territories;
+//    std::vector<Card*> cards;
+//    std::vector<Order*> orders;
+//};
+//
+//#endif
