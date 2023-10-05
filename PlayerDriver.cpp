75bdfad44c03fa262f5e17561d9df2122ace230d
Tuan Anh Pham <98975548+TuanAnh-P@users.noreply.github.com>
2023-10-05 15:57:41 -0400

    Fixed stream insertion operation for Player

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -3,9 +3,11 @@
 // Define the testPlayers function to demonstrate player object features
 void testPlayers() {
     // Create a player with the name "John"
+    std::cout << "<----------Creating Player---------->" << std::endl;
     Player* player = new Player("John");
 
     // Add territories to the player's owned territories
+    std::cout << "<----------Adding Territories---------->" << std::endl;
     player->addTerritory(new Territory("New York", 1, 2));
     player->addTerritory(new Territory("London", 3, 4));
 
@@ -13,6 +15,7 @@ void testPlayers() {
     std::vector<Territory*> territoriesToDefend = player->toDefend();
 
     // Display the territories to defend
+    std::cout << "<----------Displaying Territories To Defend---------->" << std::endl;
     std::cout << "Territories to Defend: ";
     for (const Territory* territory : territoriesToDefend) {
         std::cout << territory->getName() << " - ";
@@ -23,6 +26,7 @@ void testPlayers() {
     std::vector<Territory*> territoriesToAttack = player->toAttack();
 
     // Display the territories to attack
+    std::cout << "<----------Displaying Territories To Attack---------->" << std::endl;
     std::cout << "Territories to Attack: ";
     for (const Territory* territory : territoriesToAttack) {
         std::cout << territory->getName() << ", ";
@@ -33,24 +37,26 @@ void testPlayers() {
     OrdersList& playerOrders = player->getOrdersList();
 
     // Issue some orders
+    std::cout << "<----------Issuing Orders---------->" << std::endl;
     player->issueOrder("Airlift");
     player->issueOrder("Bomb");
 
     // Display the player's orders list
-    std::cout << "Displaying player's orders list:" << std::endl;
+    std::cout << "<----------Displaying Orders List---------->" << std::endl;
     playerOrders.print();
 
     //Use the custom operator<< to print player details
-    std::cout << player << std::endl;
+    std::cout << "<----------Player's Stats---------->" << std::endl;
+    std::cout << *player << std::endl;
 
     // Clean up the player to prevent memory leaks
+    std::cout << "<----------Deleting Player---------->" << std::endl;
     delete player;
 }
 
 //int main() {
 //    // Call the testPlayers function to demonstrate player object features
 //    testPlayers();
-//
 //    return 0;
 //}
 
