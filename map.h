c669c6ade2db4127f708829a32e6ea57d19c637e
coleblond <coltonleblond@gmail.com>
2023-11-28 20:38:17 -0500

    Adding isAdjacent function
    Check if this territory is adjacent another given territory

--- map.h
+++ map.h
@@ -22,6 +22,9 @@ public:
     
     //Setter: adds adajacent territories of the territory
     void addAdjacentTerritory(Territory* territory);
+
+    // Check if territory adjacent to another
+    bool isAdjacent(Territory* other);
     
     //Basic Getters
     std::string getName() const;
