34086be3c7d3454d509a4ef04ce5ff28e5b67ad8
Ryan Li <99624359+Ryan2Li@users.noreply.github.com>
2023-12-01 22:30:13 -0500

    Update LogObserver.cpp comments

--- LogObserver.cpp
+++ LogObserver.cpp
@@ -1,7 +1,7 @@
 #include <fstream>
 #include "LogObserver.h"
 
-
+// method to log the games
 void LogObserver::Update(ILoggable* loggable) {
     string line = loggable->stringToLog();
     std::fstream gamelog("gamelog.txt", std::ios::app);
@@ -9,6 +9,7 @@ void LogObserver::Update(ILoggable* loggable) {
     gamelog.close();
 };
 
+// method to log tournament games
 void LogObserver::UpdateTournament(ILoggable* loggable, int game) {
     string line = loggable->stringToTourLog(game);
     std::fstream gamelog("tournament.txt", std::ios::app);
